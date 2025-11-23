bbc2d1cb1d46b50be41f2640a6dc666e2730fbb7
Aden Mann <adenmann@tamu.edu>
2025-11-22 21:32:58 -0600

    Set up driver structure, introduce common sensor interface in SensorInterface.hpp
    SensorInterface.hpp provides a thorough base class that every sensor and accompanying driver can be wrapped around to provide rich and reliable functionality

---
+++ common/include/common/driver/SensorInterface.hpp
@@ -0,0 +1,111 @@
+
+#ifndef LUNABOTICS_SENSORINTERFACE_HPP_
+#define LUNABOTICS_SENSORINTERFACE_HPP_
+
+#include <cstdint>
+
+namespace lunabotics_common_sensors {
+
+/**
+ * @brief Abstract interface for all hardware sensors in the project.
+ * TODO: Improve documentation for the common sensor interface.
+ */
+class SensorInterface{
+public:
+    /**
+    * @brief Defines the current operational state of the sensor.
+     */
+    enum class SensorState : uint8_t {
+        UNINITIALIZED, // Driver created, but init() not called
+        INITIALIZED,   // init() called, but comms not confirmed
+        CONNECTED,     // Comms established, ready to read
+        DISCONNECTED,  // Communication lost
+        FAILED         // A hardware or unrecoverable error
+    };
+
+    /**
+     * @brief Defines the communication bus being used.
+     */
+    enum class InterfaceType : uint8_t {
+        I2C,  // I2C on ESP32 or Jetson 
+        SPI,  // SPI on ESP32 or Jetson
+        UART, // UART on ESP32 or Jetson
+        CAN,  // CAN on Jetson
+        USB  // USB on Jetson
+    };
+
+    /**
+     * @brief Defines which processor is directly handling the sensor driver.
+     * @note This could potentially provide future functionality where configurations leverage the ESP32 for sensor passthrough/publishing.
+     */
+    enum class HostController : uint8_t {
+        OBC, 
+        ESP32
+    };
+
+    /** 
+     * @brief Constructor to set the fixed properties of the senesor.
+     * 
+     * @param interface the bus type (I2C, SPI, etc...)
+     * @param host The host processor this driver is running on (OBC, ESP32)
+     */
+    explicit SensorInterface(InterfaceType interface, HostController host) : 
+        m_interface(interface),
+        m_host(host),
+        m_state(SensorState::UNINITIALIZED) {}
+
+    /**
+     * @brief Virtual destructor.
+     */
+    virtual ~SensorInterface() = default;
+
+    // Virtual Functions to implement in Child Sensor Drivers, these must be implemented in the Child class!
+
+    /**
+     * @brief Initializes the sensor hardware.
+     * Connects, runs self-tests, and sets configuration.
+     * Should update m_state to CONNECTED or FAILED
+     * 
+     * @return True on succesful read, false on failure
+     */
+    virtual bool init() = 0;
+
+    // Implemented Functions:
+    // These are common to all sensors and are provided by the base class.
+
+    /**
+     * @brief Gets the current state of the sensor.
+     */
+    SensorState getstate() const { return m_state; }
+
+    /**
+     * @brief Gets the interface (I2C, SPI, etc...).
+     */
+    InterfaceType getinterface() { return m_interface;}
+
+    /**
+     * @brief Gets the host controller (OBC, ESP32).
+     */
+    HostController getHost() { return m_host; }
+
+
+protected:
+    /**
+     * @brief The current state of the sensor.
+     * Child classes MUST update this variable in their init() and read() functions.
+     */
+    SensorState m_state;
+
+    /**
+     * @brief The fixed interface type for this sensor.
+     */
+    const InterfaceType m_interface;
+
+    /**
+     * @brief the fixed host controller for this sensor driver.
+     */
+    const HostController m_host;
+    
+};
+}
+#endif 
