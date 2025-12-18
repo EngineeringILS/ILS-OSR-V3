cd6ef32b009c40bcbb1240cca730d806b73b941a
Aden Mann <adenmann@tamu.edu>
2025-12-18 04:15:29 -0600

    Begin Development of I2C Driver
    Added I2C Driver component to wrap the ESP-IDF I2C Master Library.
    Registered I2C Driver and Max1704x driver in main CMakeLists.txt

---
+++ firmware/components/i2c_driver/i2c_driver.hpp
@@ -0,0 +1,66 @@
+#include <common/protocols/InterfaceProtocols.hpp>
+#include "driver/i2c_master.h"
+
+namespace Lunabotics {
+namespace ESP32 {
+namespace Drivers {
+using namespace Lunabotics::Common;
+
+class I2CBus {
+public:
+    /**
+     * @brief I2C Bus constructor that attemps initialization on construction.
+     * @param port The reference I2CPort struct to initialize.
+     */
+    explicit I2CBus(const Protocols::I2CPort &port) : port_(port) {
+        init();
+    }
+
+    ~I2CBus() {
+        if (initialized_) {
+            i2c_del_master_bus(bus_handle_);
+        }
+    }
+
+    /**
+     * @brief Attemps to initialize the I2C Bus. 
+     * Suceeds if err = ESP_OK and initialized_ = true. 
+     * Fails gracefully specifying the error code in err, and setting initialized_ to false.
+     */
+    bool init() {
+        if (!port_.exists) {
+            initialized_ = false;
+            // Port cannot be found:
+            err = ESP_ERR_NOT_FOUND;
+            
+        } else {
+            i2c_master_bus_config_t i2c_bus_config = {
+                .i2c_port = port_.i2c_port,  // select a free I2C port automatically
+                .sda_io_num = static_cast<gpio_num_t>(port_.sda_pin),
+                .scl_io_num = static_cast<gpio_num_t>(port_.scl_pin),
+                .clk_source = I2C_CLK_SRC_DEFAULT,
+                .glitch_ignore_cnt = 7,
+                .flags = { .enable_internal_pullup = true }
+            };
+
+            err = i2c_new_master_bus(&i2c_bus_config, &bus_handle_);
+            if (err == ESP_OK) {
+                initialized_ = true;
+            } else {
+                initialized_ = false;
+            }
+        }
+    return initialized_;
+    }
+
+    esp_err_t err;
+    bool initialized_ = false;
+private:
+    const Protocols::I2CPort port_;
+    i2c_master_bus_handle_t bus_handle_ = nullptr;
+   
+};
+
+}
+}
+}
--- firmware/components/max1704x_driver/Max1704x.cpp
+++ firmware/components/max1704x_driver/Max1704x.cpp
@@ -4,23 +4,23 @@ namespace Lunabotics {
 namespace Common {
 namespace Sensors{
 
-bool Max1704x::init() {
-    esp_err_t initStatus = max1704x_init_desc(
-        // device descriptor,
-        // i2c port number,
-        // sda GPIO
-        // cl GPIO
-    );
+// bool Max1704x::init() {
+//     esp_err_t initStatus = max1704x_init_desc(
+//         // device descriptor,
+//         // i2c port number,
+//         // sda GPIO
+//         // cl GPIO
+//     );
 
-    if (initStatus != ESP_OK) {
-        // TODO: Need to qualify the error to properly determine what kind of error and respective state change.
-        // Read ESP32 Docs for ESP_ERR_T status codes.
-        _State = SensorState::ERROR;
-        return false;
-    } else {
-        _State = SensorState::CONNECTED;
-    }
-}
+//     if (initStatus != ESP_OK) {
+//         // TODO: Need to qualify the error to properly determine what kind of error and respective state change.
+//         // Read ESP32 Docs for ESP_ERR_T status codes.
+//         _State = SensorState::ERROR;
+//         return false;
+//     } else {
+//         _State = SensorState::CONNECTED;
+//     }
+// }
 
 
 } // namespace Sensors
