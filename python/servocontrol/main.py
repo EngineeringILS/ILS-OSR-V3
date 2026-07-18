8059db9ca72c8cc2b0b8fd6778fc9a579473339f
Aden Mann <adenmann@engineeringils.org>
2026-07-18 04:46:56 +0000

    Fix servocontrol main.py

--- python/servocontrol/main.py
+++ python/servocontrol/main.py
@@ -8,6 +8,6 @@ from config import i2c_address, channel_count, pwm_frequency
 def main():
     servo = ServoKit(channels=channel_count, address=i2c_address, frequency=pwm_frequency)
     methods.servodriver_setup(servo=servo, pwm_frequency=pwm_frequency, i2c_address=i2c_address)
-    # methods.roboclaw_movement_loop(roboclaw=roboclaw, speed=0.5, controller_address=controller_address, debug=True)
+    methods.servo_movement_loop(servo=servo, step_degrees=1.0, channel=1, debug=True)
 if __name__ == "__main__":
     main()
